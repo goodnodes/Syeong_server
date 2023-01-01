6bdd16d58b208e9819f411dda5ddf5bd2c60edb5
96sunghyun <visioner2168@gmail.com>
2023-01-02 02:44:24 +0900

    add: controller logic

--- controller/root.go
+++ controller/root.go
@@ -12,5 +12,6 @@ type Controller struct {
 
 func GetNewController (um *model.UserModel, pm *model.PoolModel, rm *model.ReviewModel) *Controller {
 	ctl := &Controller{User : GetUserController(um, rm, pm), Pool : GetPoolController(um, rm, pm), Review : GetReviewController(um, rm, pm)} 
+	
 	return ctl
 }
